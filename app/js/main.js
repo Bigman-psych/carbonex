032ead72638cd44b206848b0bc7b3409d9549a66
Bigman-psych <sergeykyzmenko21@gmail.com>
2022-11-14 22:04:31 +0200

    company, industries pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(function () {
+
+    $('.cooperation-slider__inner').slick({
+        arrows: false,
+        slidesToShow: 5,
+       
+    });
 
 });
+// $('.top-slider__inner').slick({
+//     dots: true,
+//     fade: true,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+// });
